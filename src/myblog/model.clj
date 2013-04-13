2f46528078ee5402efe0b55ed3355dd33ec1fde3
bpn45 <bpn45@i.ua>
2013-04-13 16:28:36 +0300

    4

--- src/myblog/model.clj
+++ src/myblog/model.clj
@@ -2,27 +2,31 @@
   (:require [monger.core :as mg]
             [monger.collection :as mc]))
 
-;      Root User: 
-;   Root Password: 
-;   Database Name: 
-;Connection URL: mongodb://:/
+ ;      Root User: admin
+ ;  Root Password: CBWicSHaTQYR
+ ;  Database Name: course1task04
+ ;
+ ;Connection URL: mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/
 
 (defn dbconnect [ & local]
   (if (empty? local)
     (let [env (into {} (System/getenv))
           username "admin"
-          password "RdG-U9HgFTDE"
-          db "coursetask04"]
-  (mg/connect! {:host (get env "OPENSHIFT_MONGODB_DB_HOST")
-                     :port (get env "OPENSHIFT_MONGODB_DB_PORT")})
+          password "CBWicSHaTQYR"
+          db "course1task04"
+      host (get env "OPENSHIFT_MONGODB_DB_HOST")
+          port (Integer/parseInt (get env "OPENSHIFT_MONGODB_DB_PORT"))]
+      (when (and (string? host) (string? port) (not (= host "")))
+        (do
+  (mg/connect! {:host host
+                     :port port})
 (mg/use-db! db)
-(mg/authenticate (mg/get-db db) username (.toCharArray password))
-)
+(mg/authenticate (mg/get-db db) username (.toCharArray password))))
 (do
   (mg/connect!)
   (mg/use-db! (first local)))))
 
-(dbconnect "local")
+(dbconnect)
 
 (defn todb [item]
   (assoc item :id (inc (long (mc/count "document")))))
