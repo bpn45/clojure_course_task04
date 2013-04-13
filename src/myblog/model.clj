1c673b61425e6055d81f318a1a88f46cf93d434e
bpn45 <bpn45@i.ua>
2013-04-13 21:00:01 +0300

    5

--- src/myblog/model.clj
+++ src/myblog/model.clj
@@ -8,25 +8,24 @@
  ;
  ;Connection URL: mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/
 
-(defn dbconnect [ & local]
-  (if (empty? local)
+
     (let [env (into {} (System/getenv))
           username "admin"
           password "CBWicSHaTQYR"
           db "course1task04"
-      host (get env "OPENSHIFT_MONGODB_DB_HOST")
-          port (Integer/parseInt (get env "OPENSHIFT_MONGODB_DB_PORT"))]
-      (when (and (string? host) (string? port) (not (= host "")))
+          host  (get env "OPENSHIFT_MONGODB_DB_HOST")
+          port (get env "OPENSHIFT_MONGODB_DB_PORT")]
+      (if (not (and (nil? host) (nil? port)))
         (do
-  (mg/connect! {:host host
-                     :port port})
+  (mg/connect! {:host host   :port port})
 (mg/use-db! db)
-(mg/authenticate (mg/get-db db) username (.toCharArray password))))
+(mg/authenticate (mg/get-db db) username (.toCharArray password)))
 (do
   (mg/connect!)
-  (mg/use-db! (first local)))))
+  (mg/use-db! "local")))
+)
 
-(dbconnect)
+
 
 (defn todb [item]
   (assoc item :id (inc (long (mc/count "document")))))
